150dbce71d579e3023b71184071bb4a2af9809be
Gabriel Arteaga <gabrielarteaga71@gmail.com>
2019-09-10 16:32:58 -0700

    Added global style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import GlobalStyle from './GlobalStyle';
 
 export default function({ children }) {
   return (
     <>
+      <GlobalStyle />
       {children}
     </>
   )
